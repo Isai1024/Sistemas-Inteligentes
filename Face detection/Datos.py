4f6e4510280eaa1748d3db04f5393b3db45d2da3
Isai1024 <2130180@upv.edu.mx>
2024-05-10 17:48:29 -0600

    Face Detection

---
+++ Face detection/Datos.py
@@ -0,0 +1,44 @@
+import cv2
+import os
+import imutils
+
+personName = "Alan"
+#Url donde se encuetra las imagenes de los usuarios
+dataPath = "" 
+personPath = dataPath + '/' + personName
+
+if not os.path.exists(personPath):
+    print("Creando carpeta")
+    os.makedirs(personPath)
+
+cap = cv2.VideoCapture(0)
+
+faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+count = 0
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    if ret == False:
+        break
+    
+    frame = imutils.resize(frame, width=320)
+    gray = cv2.cvtColor(frame, 6)
+    
+    auxFrame = frame.copy()
+
+    faces = faceClassif.detectMultiScale(gray, 1.3, 5)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        rostro = auxFrame[y:y + h, x:x + w]
+        rostro = cv2.resize(rostro, (720, 720), interpolation=cv2.INTER_CUBIC)
+        cv2.imwrite(personPath + '/rostro{}.jpg'.format(count).rostro)
+        count = count + 1
+    
+    cv2.imshow('frame', frame)
+
+    if cv2.waitKey(1) == 27 or count >= 300:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
